cc669485f90a7b5f5d45abd0dd6167148b1f6914
tall-horse <dima.gamedev@pm.me>
2024-08-12 14:59:00 +0200

    bit flags & bitwise operators

--- bFlagsAndOps/Assets/AttributeManager.cs
+++ bFlagsAndOps/Assets/AttributeManager.cs
@@ -1,11 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AttributeManager : MonoBehaviour
 {
+    static public int MAGIC = 16;
+    static public int INTELLIGENCE = 8;
+    static public int CHARISMA = 4;
+    static public int FLY = 2;
+    static public int INVISIBLE = 1;
+
     public Text attributeDisplay;
+    int attributes = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +23,31 @@ public class AttributeManager : MonoBehaviour
     {
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
         attributeDisplay.transform.position = screenPoint + new Vector3(0,-50,0);
+        attributeDisplay.text = Convert.ToString(attributes, 2).PadLeft(8, '0');
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.CompareTag("MAGIC"))
+        {
+            attributes |= MAGIC;
+        }
+        if(other.gameObject.CompareTag("INTELLIGENCE"))
+        {
+            attributes |= INTELLIGENCE;
+        }
+        if(other.gameObject.CompareTag("CHARISMA"))
+        {
+            attributes |= CHARISMA;
+        }
+        if(other.gameObject.CompareTag("FLY"))
+        {
+            attributes |= FLY;
+        }
+        if(other.gameObject.CompareTag("INVISIBLE"))
+        {
+            attributes |= INVISIBLE;
+        }
     }
        
 }
